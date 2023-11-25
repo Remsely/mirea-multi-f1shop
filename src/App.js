deffc54ea8f3d0ae6efc04bf26345ad15f64ffc6
Remsely <prokopchukromain@mail.ru>
2023-11-25 21:47:00 +0300

    fix: попытка исправить 404 после обновления страницы

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './styles/App.css';
-import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/UI/NavBar/NavBar";
 import Footer from "./components/UI/footer/Footer";
@@ -7,11 +6,9 @@ import Footer from "./components/UI/footer/Footer";
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
-                <NavBar/>
-                <AppRouter/>
-                <Footer/>
-            </BrowserRouter>
+            <NavBar/>
+            <AppRouter/>
+            <Footer/>
         </div>
     );
 }
