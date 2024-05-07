0befbd41ed160f160e48c17a5ed0e4907201bfdc
Remsely <prokopchukromain@mail.ru>
2024-05-07 19:15:00 +0300

    feat: add spring boot app

---
+++ back/src/main/java/edu/remsely/f1shop/F1shopApplication.java
@@ -0,0 +1,11 @@
+package edu.remsely.f1shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1shopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(F1shopApplication.class, args);
+    }
+}
